3e5b58411bbf7e85700c439bbed04bd29a987d2e
lucas <var.lcsjes@gmail.com>
2023-11-09 02:09:01 -0300

    3/19 "ServidorConExpress"

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const express = require("express");
 const ProductManager = require("./ProductManager");
 
-const productManager = new ProductManager("products.json");
+const app = express();
+const port = 3000; // Puedes cambiar el puerto si es necesario
+const productManager = new ProductManager("./products.json"); // Asegúrate de que 'products.json' existe y tiene algunos productos
 
 // Agregar productos
 productManager.addProduct({
@@ -42,3 +45,40 @@ productManager.deleteProduct(2);
 // Obtener y mostrar todos los productos después de la eliminación
 const remainingProducts = productManager.getProducts();
 console.log("Productos restantes:", remainingProducts);
+
+// Endpoint para obtener todos los productos con límite opcional
+app.get("/products", async (req, res) => {
+  try {
+    const limit = req.query.limit;
+    const products = await productManager.getProducts();
+
+    if (limit) {
+      res.json(products.slice(0, limit));
+    } else {
+      res.json(products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener productos." });
+  }
+});
+
+// Endpoint para obtener un producto por ID
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: "Producto no encontrado." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener el producto." });
+  }
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor Express iniciado en http://localhost:${port}`);
+});
